c847b674e44307f1fad845644cf88895f9c953c8
unozer <40232537+unozer@users.noreply.github.com>
2025-04-11 13:02:58 +0200

    binding dati + suggerimenti

--- script.js
+++ script.js
@@ -5,6 +5,29 @@ const meteoIcona = document.querySelector('.meteo-icona');
 const meteoTemeperatura = document.querySelector('.meteo-temperatura');
 const meteoSuggerimenti = document.querySelector('.meteo-suggerimenti');
 
+// dichiaro array di suggerimenti
+const suggestions = {
+    '01d': 'Ricordati la crema solare!',
+    '01n': 'Buonanotte!',
+    '02d': 'Oggi il sole va e viene...',
+    '02n': 'Attenti ai lupi mannari...',
+    '03d': 'Luce perfetta per fare foto!',
+    '03n': 'Dormi sereno :)',
+    '04d': 'Che cielo grigio :(',
+    '04n': 'Non si vede nemmeno la luna!',
+    '09d': 'Prendi l\'ombrello',
+    '09n': 'Copriti bene!',
+    '10d': 'Prendi l\'ombrello',
+    '10n': 'Copriti bene!',
+    '11d': 'Attento ai fulmini!',
+    '11n': 'I lampi accendono la notte!',
+    '13d': 'Esci a fare un pupazzo di neve!',
+    '13n': 'Notte perfetta per stare sotto il piumone!',
+    '50d': 'Accendi i fendinebbia!',
+    '50n': 'Guida con prudenza!'
+};
+  
+
 // Provo a recuperare la posizione
 navigator.geolocation.getCurrentPosition(on_success, on_error);
 
@@ -36,4 +59,18 @@ async function on_success(position) {
     const data = await response.json();
 
     console.log(data);
+
+    // Recupero dei dati dalla response
+    const icon = data.weather[0].icon;
+    const description = data.weather[0].description;
+
+    // Binding dati sulla UI
+    meteoLocation.innerText = data.name;
+    meteoIcona.src = `images/${icon}.png`;
+    meteoIcona.alt = description;
+    meteoTemeperatura.innerText = Math.floor(data.main.temp) + "°C";
+    meteoSuggerimenti.innerText = suggestions[icon];
+
+    // disattiva il loading
+    htmlElement.className = '';
 }
